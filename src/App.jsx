ca744857f240da8871412137121db7a2a23d129c
Muhammad Dimas W <mdimaswakhid@gmail.com>
2026-06-11 13:56:51 +0700

    Penambahanan beberapa fitur

--- src/App.jsx
+++ src/App.jsx
@@ -14,38 +14,24 @@ import UpdateTicketDetail from "./pages/UpdateTicketDetail";
 
 function App() {
   return (
-    <Router>
+    // ⚠️ Tambahkan basename di sini agar otomatis membaca subfolder GitHub Pages
+    <Router basename="/crm-trouble-ticket">
       <Routes>
         {/* Halaman Login tanpa Sidebar */}
-        <Route path="/crm-trouble-ticket/" element={<Login />} />
+        <Route path="/" element={<Login />} />
 
         {/* Halaman-halaman dengan Sidebar (Layout) */}
-        <Route path="/crm-trouble-ticket/admin" element={<AdminDashboard />} />
-        <Route
-          path="/crm-trouble-ticket/user-dashboard"
-          element={<UserDashboard />}
-        />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
 
         {/* Management User */}
-        <Route path="/crm-trouble-ticket/users" element={<UserList />} />
-        <Route
-          path="/crm-trouble-ticket/create-user"
-          element={<CreateUser />}
-        />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/create-user" element={<CreateUser />} />
 
         {/* Management Tiket */}
-        <Route
-          path="/crm-trouble-ticket/open-ticket"
-          element={<OpenTicket />}
-        />
-        <Route
-          path="/crm-trouble-ticket/update-ticket"
-          element={<UpdateTicketList />}
-        />
-        <Route
-          path="/crm-trouble-ticket/update-ticket/:id"
-          element={<UpdateTicketDetail />}
-        />
+        <Route path="/open-ticket" element={<OpenTicket />} />
+        <Route path="/update-ticket" element={<UpdateTicketList />} />
+        <Route path="/update-ticket/:id" element={<UpdateTicketDetail />} />
       </Routes>
     </Router>
   );
